bb3c25c069c908e77d061d664dcd7506d17db6b0
Nishchay Jain <33507751+nishchayjain@users.noreply.github.com>
2018-12-24 23:50:04 +0530

    Counter updated with accurate days remaining

--- js/count.js
+++ js/count.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = '2019-02-23';
 initializeClock('clockdiv', deadline);
